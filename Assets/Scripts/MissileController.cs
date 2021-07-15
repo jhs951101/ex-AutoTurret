7dadb636e7277db1586d98b87747953e49107f43
spg1101 <sharping@SharPingui-MacBookPro.local>
2021-07-16 02:47:23 +0900

    m

---
+++ Assets/Scripts/MissileController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MissileController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private GameObject target = null;
+    private Transform targetTransform = null;
+    private float stoppingDistance = 0;
+
+    private void Update()
+    {
+        if (targetTransform != null)
+        {
+            if (Vector3.Distance(transform.position, targetTransform.position) > stoppingDistance)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetTransform.position, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    public void setTargetPosition(Transform targetPosition)
+    {
+        targetTransform = targetPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject missileObject;
+
+    [SerializeField]
+    private Transform tfGunBody;
+
+    [SerializeField]
+    private Transform missilePlace;
+
+    [SerializeField]
+    private LayerMask enemyLayerMask;
+
+    [SerializeField]
+    private float shootingRange;
+
+    [SerializeField]
+    private float rotateTimes;
+
+    [SerializeField]
+    private float spinAttackSpeed;
+
+    [SerializeField]
+    private float fireRate;
+
+    private Transform tfTarget;
+    private float currentFireRate;
+
+    private void Start()
+    {
+        tfTarget = null;
+        currentFireRate = fireRate;
+        InvokeRepeating("SearchEnemy", 0f, 0.5f);
+    }
+
+    private void Update()
+    {
+        if(tfTarget == null)
+        {
+            tfGunBody.Rotate(new Vector3(0, rotateTimes * 45, 0) * Time.deltaTime);
+        }
+        else
+        {
+            Quaternion t_lookRotation = Quaternion.LookRotation(tfTarget.position);
+            Vector3 t_euler = Quaternion.RotateTowards(tfGunBody.rotation, t_lookRotation, spinAttackSpeed * Time.deltaTime).eulerAngles;
+            tfGunBody.rotation = Quaternion.Euler(0, t_euler.y, 0);
+
+            Quaternion t_fireRotation = Quaternion.Euler(0, t_lookRotation.eulerAngles.y, 0);
+            if(Quaternion.Angle(tfGunBody.rotation, t_fireRotation) < 5f)
+            {
+                currentFireRate -= Time.deltaTime;
+                if (currentFireRate <= 0)
+                {
+                    currentFireRate = fireRate;
+
+                    if (tfTarget != null)
+                    {
+                        GameObject missile = Instantiate(missileObject, missilePlace.position, Quaternion.identity);
+                        missile.GetComponent<MissileController>().setTargetPosition(tfTarget);
+                    }
+                }
+            }
+        }
+    }
+
+    private void SearchEnemy()
+    {
+        Collider[] t_cols = Physics.OverlapSphere(transform.position, shootingRange, enemyLayerMask);
+        Transform t_shortestTarget = null;
+
+        if (t_cols.Length > 0)
+        {
+            float t_shortestDistance = Mathf.Infinity;
+            foreach (Collider t_colTarget in t_cols)
+            {
+                float t_distance = Vector3.SqrMagnitude(transform.position - t_colTarget.transform.position);
+                if (t_shortestDistance > t_distance)
+                {
+                    t_shortestDistance = t_distance;
+                    t_shortestTarget = t_colTarget.transform;
+                }
+            }
+        }
+
+        tfTarget = t_shortestTarget;
+    }
+}
